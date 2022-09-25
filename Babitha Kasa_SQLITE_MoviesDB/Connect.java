87c8063b8f8c5fd70b8d76599c9d88df32995bcc
BABITHA KASA <85307674+babithakasa42@users.noreply.github.com>
2022-09-26 01:29:42 +0530

    Update Connect.java

--- Babitha Kasa_SQLITE_MoviesDB/Connect.java
+++ Babitha Kasa_SQLITE_MoviesDB/Connect.java
@@ -13,7 +13,7 @@ public class Connect {
         Connection conn = null;
         try {
             // db parameters
-            String url = "jdbc:sqlite:C:/sqlite/db/SANDEEP.db";
+            String url = "jdbc:sqlite:C:/sqlite/db/BABITHA.db";
             // create a connection to the database
             conn = DriverManager.getConnection(url);
             
